6a31c2dda75e32f830e71306016811031add82b0
Devin Hayward <devin.hayward@icloud.com>
2020-09-27 13:19:53 -0400

    basic board working

--- Sources/SodukuBeDamned/SodukuBeDamned.swift
+++
@@ -1,3 +0,0 @@
-struct SodukuBeDamned {
-    var text = "Hello, World!"
-}
--- Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
+++ Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
@@ -6,7 +6,9 @@ final class SodukuBeDamnedTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SodukuBeDamned().text, "Hello, World!")
+        var board = Board()
+        board.box5.space4 = 5
+        XCTAssertEqual(board.box5.space4, 5)
     }
 
     static var allTests = [
