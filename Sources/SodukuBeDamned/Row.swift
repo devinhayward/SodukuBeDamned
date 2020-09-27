6b37dd6e09abb2b268beea04afadef242febb57a
Devin Hayward <devin.hayward@icloud.com>
2020-09-27 17:10:16 -0400

    added row functions and tests up to Row 7

--- Sources/SodukuBeDamned/Row.swift
+++ Sources/SodukuBeDamned/Row.swift
@@ -41,4 +41,77 @@ public extension Board {
         
         return final
     }
+    
+    func Row4() -> [Int] {
+        let rowA = [box4.space1, box4.space2, box4.space3]
+        let rowB = [box5.space1, box5.space2, box5.space3]
+        let rowC = [box6.space1, box6.space2, box6.space3]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    func Row5() -> [Int] {
+        let rowA = [box4.space4, box4.space5, box4.space6]
+        let rowB = [box5.space4, box5.space5, box5.space6]
+        let rowC = [box6.space4, box6.space5, box6.space6]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    func Row6() -> [Int] {
+        let rowA = [box4.space7, box4.space8, box4.space9]
+        let rowB = [box5.space7, box5.space8, box5.space9]
+        let rowC = [box6.space7, box6.space8, box6.space9]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    func Row7() -> [Int] {
+        let rowA = [box7.space1, box7.space2, box7.space3]
+        let rowB = [box8.space1, box8.space2, box8.space3]
+        let rowC = [box9.space1, box9.space2, box9.space3]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    func Row8() -> [Int] {
+        let rowA = [box7.space4, box7.space5, box7.space6]
+        let rowB = [box8.space4, box8.space5, box8.space6]
+        let rowC = [box9.space4, box9.space5, box9.space6]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    func Row9() -> [Int] {
+        let rowA = [box7.space7, box7.space8, box7.space9]
+        let rowB = [box8.space7, box8.space8, box8.space9]
+        let rowC = [box9.space7, box9.space8, box9.space9]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
 }
--- Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
+++ Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
@@ -27,9 +27,42 @@ final class SodukuBeDamnedTests: XCTestCase {
         XCTAssertEqual(board.Row3(), [7,0,0,0,8,0,0,0,9])
     }
     
+    func testRow4() {
+        var board = Board()
+        board.box4.space1 = 1
+        board.box5.space2 = 2
+        board.box6.space3 = 3
+        XCTAssertEqual(board.Row4(), [1,0,0,0,2,0,0,0,3])
+    }
+    
+    func testRow5() {
+        var board = Board()
+        board.box4.space4 = 4
+        board.box5.space5 = 5
+        board.box6.space6 = 6
+        XCTAssertEqual(board.Row5(), [4,0,0,0,5,0,0,0,6])
+    }
+    
+    func testRow6() {
+        var board = Board()
+        board.box4.space7 = 7
+        board.box5.space8 = 8
+        board.box6.space9 = 9
+        XCTAssertEqual(board.Row6(), [7,0,0,0,8,0,0,0,9])
+    }
+    
+    func testRow7() {
+        var board = Board()
+        board.box7.space1 = 1
+        board.box8.space2 = 2
+        board.box9.space3 = 3
+        XCTAssertEqual(board.Row7(), [1,0,0,0,2,0,0,0,3])
+    }
     
 
     static var allTests = [
-        ("testBoardSet", testBoardSet), ("testRow2", testRow2), ("testRow3", testRow3)
+        ("testBoardSet", testBoardSet), ("testRow2", testRow2), ("testRow3", testRow3),
+        ("testRow4", testRow4), ("testRow5", testRow5), ("testRow6", testRow6),
+        ("testRow7", testRow7)
     ]
 }
