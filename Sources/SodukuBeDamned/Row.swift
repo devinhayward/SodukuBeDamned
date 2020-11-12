b4cdf422caced6201c54af82a8af4392152d3455
Devin Hayward <devin.hayward@icloud.com>
2020-11-11 20:28:35 -0500

    Finished the Row implementation and all its tests 👍✅

--- Sources/SodukuBeDamned/Row.swift
+++ Sources/SodukuBeDamned/Row.swift
@@ -57,67 +57,68 @@ public extension Board {
         return final
     }
     
-//    var Row5: Row {
-//        let rowA = [box4.space4, box4.space5, box4.space6]
-//        let rowB = [box5.space4, box5.space5, box5.space6]
-//        let rowC = [box6.space4, box6.space5, box6.space6]
-//        
-//        var final = rowA
-//        final.append(contentsOf: rowB)
-//        final.append(contentsOf: rowC)
-//        
-//        return final
-//    }
-//    
-//    var Row6: Row {
-//        let rowA = [box4.space7, box4.space8, box4.space9]
-//        let rowB = [box5.space7, box5.space8, box5.space9]
-//        let rowC = [box6.space7, box6.space8, box6.space9]
-//        
-//        var final = rowA
-//        final.append(contentsOf: rowB)
-//        final.append(contentsOf: rowC)
-//        
-//        return final
-//    }
-//    
-//    var Row7: Row {
-//        let rowA = [box7.space1, box7.space2, box7.space3]
-//        let rowB = [box8.space1, box8.space2, box8.space3]
-//        let rowC = [box9.space1, box9.space2, box9.space3]
-//        
-//        var final = rowA
-//        final.append(contentsOf: rowB)
-//        final.append(contentsOf: rowC)
-//        
-//        return final
-//    }
-//    
-//    var Row8: Row {
-//        let rowA = [box7.space4, box7.space5, box7.space6]
-//        let rowB = [box8.space4, box8.space5, box8.space6]
-//        let rowC = [box9.space4, box9.space5, box9.space6]
-//        
-//        var final = rowA
-//        final.append(contentsOf: rowB)
-//        final.append(contentsOf: rowC)
-//        
-//        return final
-//    }
-//    
-//    var Row9: Row {
-//        let rowA = [box7.space7, box7.space8, box7.space9]
-//        let rowB = [box8.space7, box8.space8, box8.space9]
-//        let rowC = [box9.space7, box9.space8, box9.space9]
-//        
-//        var final = rowA
-//        final.append(contentsOf: rowB)
-//        final.append(contentsOf: rowC)
-//        
-//        return final
-//    }
+    var Row5: Row {
+        let rowA = [boxState[BoxNames.box4]!.space4,boxState[BoxNames.box4]!.space5,boxState[BoxNames.box4]!.space6]
+        let rowB = [boxState[BoxNames.box5]!.space4,boxState[BoxNames.box5]!.space5,boxState[BoxNames.box5]!.space6]
+        let rowC = [boxState[BoxNames.box6]!.space4,boxState[BoxNames.box6]!.space5,boxState[BoxNames.box6]!.space6]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    var Row6: Row {
+        let rowA = [boxState[BoxNames.box4]!.space7,boxState[BoxNames.box4]!.space8,boxState[BoxNames.box4]!.space9]
+        let rowB = [boxState[BoxNames.box5]!.space7,boxState[BoxNames.box5]!.space8,boxState[BoxNames.box5]!.space9]
+        let rowC = [boxState[BoxNames.box6]!.space7,boxState[BoxNames.box6]!.space8,boxState[BoxNames.box6]!.space9]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    var Row7: Row {
+        let rowA = [boxState[BoxNames.box7]!.space1,boxState[BoxNames.box7]!.space2,boxState[BoxNames.box7]!.space3]
+        let rowB = [boxState[BoxNames.box8]!.space1,boxState[BoxNames.box8]!.space2,boxState[BoxNames.box8]!.space3]
+        let rowC = [boxState[BoxNames.box9]!.space1,boxState[BoxNames.box9]!.space2,boxState[BoxNames.box9]!.space3]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    var Row8: Row {
+        let rowA = [boxState[BoxNames.box7]!.space4,boxState[BoxNames.box7]!.space5,boxState[BoxNames.box7]!.space6]
+        let rowB = [boxState[BoxNames.box8]!.space4,boxState[BoxNames.box8]!.space5,boxState[BoxNames.box8]!.space6]
+        let rowC = [boxState[BoxNames.box9]!.space4,boxState[BoxNames.box9]!.space5,boxState[BoxNames.box9]!.space6]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
+    
+    var Row9: Row {
+        let rowA = [boxState[BoxNames.box7]!.space7,boxState[BoxNames.box7]!.space8,boxState[BoxNames.box7]!.space9]
+        let rowB = [boxState[BoxNames.box8]!.space7,boxState[BoxNames.box8]!.space8,boxState[BoxNames.box8]!.space9]
+        let rowC = [boxState[BoxNames.box9]!.space7,boxState[BoxNames.box9]!.space8,boxState[BoxNames.box9]!.space9]
+        
+        var final = rowA
+        final.append(contentsOf: rowB)
+        final.append(contentsOf: rowC)
+        
+        return final
+    }
     
     var rows: [Rows:Row] {
-        return [Rows.row1:Row1, Rows.row2:Row2, Rows.row3:Row3]
+        return [Rows.row1:Row1, Rows.row2:Row2, Rows.row3:Row3, Rows.row4:Row4, Rows.row5:Row5, Rows.row6:Row6,
+                Rows.row7:Row7, Rows.row8:Row8, Rows.row9:Row9]
     }
 }
--- Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
+++ Tests/SodukuBeDamnedTests/SodukuBeDamnedTests.swift
@@ -61,51 +61,111 @@ final class SodukuBeDamnedTests: XCTestCase {
         }), [1,0,0,0,5,0,0,0,3])
     }
 
+    func testRow5() {
+        let box4 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 4, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box5 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 8, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box6 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 2, sp7: 0, sp8: 0, sp9: 0)
+        
+        let dmhInitialState = [Board.BoxNames.box1:Box(), Board.BoxNames.box2:Box(), Board.BoxNames.box3:Box(), Board.BoxNames.box4:box4,
+                               Board.BoxNames.box5:box5, Board.BoxNames.box6:box6, Board.BoxNames.box7:Box(), Board.BoxNames.box8:Box(), Board.BoxNames.box9:Box()]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        XCTAssertEqual(board.Row5.map({ space in
+            return space.finalValue
+        }), [4,0,0,0,8,0,0,0,2])
+    }
+    
+    func testRow6() {
+        let box4 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 7, sp8: 0, sp9: 0)
+        let box5 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 1, sp9: 0)
+        let box6 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 6, sp9: 0)
+        
+        let dmhInitialState = [Board.BoxNames.box1:Box(), Board.BoxNames.box2:Box(), Board.BoxNames.box3:Box(), Board.BoxNames.box4:box4,
+                               Board.BoxNames.box5:box5, Board.BoxNames.box6:box6, Board.BoxNames.box7:Box(), Board.BoxNames.box8:Box(), Board.BoxNames.box9:Box()]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        XCTAssertEqual(board.Row6.map({ space in
+            return space.finalValue
+        }), [7,0,0,0,1,0,0,6,0])
+    }
+    
+    func testRow7() {
+        let box7 = Box(sp1: 6, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box8 = Box(sp1: 0, sp2: 4, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box9 = Box(sp1: 0, sp2: 3, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        
+        let dmhInitialState = [Board.BoxNames.box1:Box(), Board.BoxNames.box2:Box(), Board.BoxNames.box3:Box(), Board.BoxNames.box4:Box(),
+                               Board.BoxNames.box5:Box(), Board.BoxNames.box6:Box(), Board.BoxNames.box7:box7, Board.BoxNames.box8:box8, Board.BoxNames.box9:box9]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        XCTAssertEqual(board.Row7.map({ space in
+            return space.finalValue
+        }), [6,0,0,0,4,0,0,3,0])
+    }
 
+    func testRow8() {
+        let box7 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 2, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box8 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 9, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 0)
+        let box9 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 7, sp7: 0, sp8: 0, sp9: 0)
+        
+        let dmhInitialState = [Board.BoxNames.box1:Box(), Board.BoxNames.box2:Box(), Board.BoxNames.box3:Box(), Board.BoxNames.box4:Box(),
+                               Board.BoxNames.box5:Box(), Board.BoxNames.box6:Box(), Board.BoxNames.box7:box7, Board.BoxNames.box8:box8, Board.BoxNames.box9:box9]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        XCTAssertEqual(board.Row8.map({ space in
+            return space.finalValue
+        }), [0,2,0,9,0,0,0,0,7])
+    }
+
+    func testRow9() {
+        let box7 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 2, sp9: 0)
+        let box8 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 9, sp8: 0, sp9: 0)
+        let box9 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 4)
+        
+        let dmhInitialState = [Board.BoxNames.box1:Box(), Board.BoxNames.box2:Box(), Board.BoxNames.box3:Box(), Board.BoxNames.box4:Box(),
+                               Board.BoxNames.box5:Box(), Board.BoxNames.box6:Box(), Board.BoxNames.box7:box7, Board.BoxNames.box8:box8, Board.BoxNames.box9:box9]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        XCTAssertEqual(board.Row9.map({ space in
+            return space.finalValue
+        }), [0,2,0,9,0,0,0,0,4])
+    }
+    
+    // test the rows Dictionary; really Rows should be used only where a Collection of rows is required. Implemented differently in this test
+    // but still confirms that it is working.
+    func testRows() {
+        let box1 = Box(sp1: 1, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 2, sp9: 0)
+        let box2 = Box(sp1: 0, sp2: 2, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 9, sp8: 0, sp9: 0)
+        let box3 = Box(sp1: 0, sp2: 0, sp3: 3, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 4)
+        
+        let box7 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 2, sp9: 0)
+        let box8 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 9, sp8: 0, sp9: 0)
+        let box9 = Box(sp1: 0, sp2: 0, sp3: 0, sp4: 0, sp5: 0, sp6: 0, sp7: 0, sp8: 0, sp9: 4)
+        
+        let dmhInitialState = [Board.BoxNames.box1:box1, Board.BoxNames.box2:box2, Board.BoxNames.box3:box3, Board.BoxNames.box4:Box(),
+                               Board.BoxNames.box5:Box(), Board.BoxNames.box6:Box(), Board.BoxNames.box7:box7, Board.BoxNames.box8:box8, Board.BoxNames.box9:box9]
+        
+        let board = Board(initialBoard: dmhInitialState)
+        
+        let row1 = board.rows[Board.Rows.row1]!.map { space in
+            return space.finalValue
+        }
+        
+        let row9 = board.rows[Board.Rows.row9]!.map { space in
+            return space.finalValue
+        }
+        
+        XCTAssertEqual(row1, [1,0,0,0,2,0,0,0,3])
+        XCTAssertEqual(row9, [0,2,0,9,0,0,0,0,4])
+    }
     
     
-    
-    
-//    func testRow5() {
-//        var board = Board()
-//        board.box4.space4.currentValue = 4
-//        board.box5.space5.currentValue = 5
-//        board.box6.space6.currentValue = 6
-//        XCTAssertEqual(board.Row5, [4,0,0,0,5,0,0,0,6])
-//    }
-//
-//    func testRow6() {
-//        var board = Board()
-//        board.box4.space7.currentValue = 7
-//        board.box5.space8.currentValue = 8
-//        board.box6.space9.currentValue = 9
-//        XCTAssertEqual(board.Row6, [7,0,0,0,8,0,0,0,9])
-//    }
-//
-//    func testRow7() {
-//        var board = Board()
-//        board.box7.space1.currentValue = 1
-//        board.box8.space2.currentValue = 2
-//        board.box9.space3.currentValue = 3
-//        XCTAssertEqual(board.Row7, [1,0,0,0,2,0,0,0,3])
-//    }
-//
-//    func testRow8() {
-//        var board = Board()
-//        board.box7.space4.currentValue = 4
-//        board.box8.space5.currentValue = 5
-//        board.box9.space6.currentValue = 6
-//        XCTAssertEqual(board.Row8, [4,0,0,0,5,0,0,0,6])
-//    }
-//
-//    func testRow9() {
-//        var board = Board()
-//        board.box7.space7.currentValue = 7
-//        board.box8.space8.currentValue = 8
-//        board.box9.space9.currentValue = 9
-//        XCTAssertEqual(board.Row9, [7,0,0,0,8,0,0,0,9])
-//    }
-//
+
 //    func testCol1() {
 //        var board = Board()
 //        board.box1.space1.currentValue = 1
@@ -139,9 +199,9 @@ final class SodukuBeDamnedTests: XCTestCase {
 //    }
 
     static var allTests = [
-        ("testBoardSet", testBoardSet), ("testRow2", testRow2), ("testRow3", testRow3)//,
-//        ("testRow4", testRow4), ("testRow5", testRow5), ("testRow6", testRow6),
-//        ("testRow7", testRow7), ("testRow8", testRow8), ("testRow9", testRow9),
+        ("testBoardSet", testBoardSet), ("testRow2", testRow2), ("testRow3", testRow3),
+        ("testRow4", testRow4), ("testRow5", testRow5), ("testRow6", testRow6),
+        ("testRow7", testRow7), ("testRow8", testRow8), ("testRow9", testRow9), ("testRows", testRows)
 //        ("testCol1", testCol1), ("testCol2", testCol2), ("testCol3", testCol3),
 //        ("testCol4", testCol4)
     ]
